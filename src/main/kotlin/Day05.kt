fd57bb4dfc352ff498b2be83548a5b7681b323ef
Vladas Maier <v.maier@dracoon.com>
2023-12-08 20:00:38 +0100

    feat: parse maps for day 5

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -11,13 +11,21 @@ fun solveDay5Part1(): Int {
         .split(" ")
         .filter { it.isNotBlank() }
         .map(String::toInt)
-
-    val maps = input
+    val mapList = input
         .dropWhile { !it.startsWith("seeds:") }
-        .drop(1)
-        .filter { line -> line.isNotEmpty() }
-        // TODO
-
-
+        .drop(2)
+    val maps = mutableMapOf<String, List<List<Int>>>()
+    var i = 0
+    while (i < mapList.size) {
+        val key = mapList[i].split(" ")[0]
+        val value = mutableListOf<List<Int>>()
+        var j = 1
+        while (mapList.getOrNull(i + j)?.isNotEmpty() == true) {
+            value.add(mapList[i + j].split(" ").map(String::toInt))
+            j++
+        }
+        maps[key] = value
+        i += j + 1
+    }
     return 0
 }
