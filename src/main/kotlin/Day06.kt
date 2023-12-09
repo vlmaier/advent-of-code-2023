3c3354e67210634604d8f7e5555dd85d7893785f
Vladas Maier <v.maier@dracoon.com>
2023-12-09 20:13:24 +0100

    feat: solve day 6

---
+++ src/main/kotlin/Day06.kt
@@ -0,0 +1,43 @@
+/**
+ * --- Day 6: Wait For It ---
+ */
+fun main() {
+    solveDay6Part1().let { println("Part 1: $it") }
+    solveDay6Part2().let { println("Part 2: $it") }
+}
+
+fun solveDay6Part1(): Int {
+    val input = readInput("day06")
+    val times = extractLine(input[0])
+    val distances = extractLine(input[1])
+    val raceResults = times.zip(distances) { time, distance ->
+        calculateWaysToBeatRecord(time, distance)
+    }
+    return raceResults.reduce(Int::times)
+}
+
+fun solveDay6Part2(): Int {
+    val input = readInput("day06")
+    val time = extractLine(input[0]).let { concatLine(it) }
+    val distance = extractLine(input[1]).let { concatLine(it) }
+    return calculateWaysToBeatRecord(time, distance)
+}
+
+private fun extractLine(input: String): List<Long> {
+    return input.split("\\s+".toRegex()).filter { it.isNotBlank() }.drop(1).map { it.toLong() }
+}
+
+private fun concatLine(input: List<Long>): Long {
+    return input.joinToString(" ").replace(" ", "").toLong()
+}
+
+private fun calculateWaysToBeatRecord(time: Long, distance: Long): Int {
+    var ways = 0
+    for (holdTime in 0..time) {
+        val traveledDistance = holdTime * (time - holdTime)
+        if (traveledDistance > distance) {
+            ways++
+        }
+    }
+    return ways
+}
