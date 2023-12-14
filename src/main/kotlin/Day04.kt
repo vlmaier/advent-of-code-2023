50ea71ce19a31bdc2b1c68b6c707969369ec6efc
Vladas Maier <v.maier@dracoon.com>
2023-12-14 12:23:18 +0100

    feat: solve day 4 part 2

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -70,12 +70,13 @@
  * Process all of the original and copied scratchcards until no more scratchcards are won. Including the original set of scratchcards, how many total scratchcards do you end up with?
  */
 fun main() {
-    solveDay4Part1().let { println("Part 1: $it") }
+    val input = readInput("day04")
+    solveDay4Part1(input).let { println("Part 1: $it") }
+    solveDay4Part2(input).let { println("Part 2: $it") }
 }
 
-fun solveDay4Part1(): Int {
-    val input = readInput("day04")
-    var points = 0
+fun solveDay4Part1(input: List<String>): Long {
+    var points = 0L
     for (line in input) {
         val card = Card(line)
         val countWinningNumbers = card.countWinningNumbers()
@@ -86,6 +87,27 @@ fun solveDay4Part1(): Int {
         points += pointsPerCard
     }
     return points
+}
+
+fun solveDay4Part2(input: List<String>): Long {
+    val winningCards = emptyList<String>().toMutableList()
+    for (line in input) {
+        processCard(input, winningCards, input.indexOf(line))
+    }
+    return winningCards.size.toLong()
+}
+
+fun processCard(allCards: List<String>, winningCards: MutableList<String>, cardIndex: Int = 0) {
+    if (cardIndex >= allCards.size) {
+        return
+    }
+    val line = allCards[cardIndex]
+    val card = Card(line)
+    val countWinningNumbers = card.countWinningNumbers()
+    winningCards.add(line)
+    for (i in 1..countWinningNumbers) {
+        processCard(allCards, winningCards, i + cardIndex)
+    }
 }
 
 data class Card(val input: String) {
